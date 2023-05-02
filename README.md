7d668a9289f0c5bde8b2ae77fd8b8d632adb44e4
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-05-02 19:40:35 -0400

    adds forEach

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Phase-1-Project
-Phase-1-project creating an application to educate first time pet owners about their new guests.
+# Phase-1 Project
+Phase-1 project creating an application to educate first time cat owners about their new guests.
+
--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 console.log("main.js works") // Test used to make sure the html file is connected to the json file 
 fetch("http://localhost:3000/cats")
 .then(response=>response.json())
-.then(json => console.log(json))
+.then(cats => cats.forEach(cat => renderCat(cat)))
+
+
+function renderCat(cat){
+  // Make an img 
+  const image = document.createElement("img");
+  image.src = cat.Image
+const ul = document.getElementById("catList")
+console.log(ul)
+ul.appendChild(image)
+}
+
