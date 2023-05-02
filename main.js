eb0a11805f6f0048d6ea07b0b3e25c322dc01c5e
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-05-02 12:49:15 -0400

    test fetch cat images works

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 console.log("main.js works") // Test used to make sure the html file is connected to the json file 
+fetch("http://localhost:3000/cats")
+.then(response=>response.json())
+.then(json => console.log(json))
