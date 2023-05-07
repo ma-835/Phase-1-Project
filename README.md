f66ad5e4f970fb4050ff014ab8762eff26032d5f
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-05-07 11:55:37 -0400

    function renders all cats

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Phase-1 Project
-Phase-1 project creating an application to educate first time cat owners about their new guests.
+Phase-1 project creating an application to educate and quiz first time cat owners about their new guests.
+The website consists of 5 different topics illustrated by the cat images,facts and questions . A user will interact with each image and will test their knowledge of different aspects of their cat care skills . Questions will consist of three options , once a user tests their skill and answers the question , a button could then be clicked to reveal a dropdown with the correct answer . 
 
+Project Name
+Pawfect CatCare Skills 
+
+Table of Contents
+Installation
+Usage
+Contributing
+License
+Installation
+Instructions for how to install the project and any dependencies.
+
+Usage
+Instructions for how to use the project, along with any examples or screenshots.
+
+Contributing
+Information for how others can contribute to the project, including guidelines for submitting pull requests or reporting issues.
+
+License
+Information about the project's license.
+
+Contact
+How to get in touch with the project maintainer(s) if you have questions or feedback.
+
+Acknowledgements
+Any acknowledgements or credits for external libraries, resources, or contributors.
+
+Additional sections
--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-console.log("main.js works") // Test used to make sure the html file is connected to the json file 
-fetch("http://localhost:3000/cats")
+// document.addEventListener("DOMContentLoaded", function() {}
+//console.log("main.js works") // Test used to make sure the html file is connected to the json file 
+
+
+fetch(`http://localhost:3000/cats`)
 .then(response=>response.json())
 .then(cats => cats.forEach(cat => renderCat(cat)))
 
-
-function renderCat(cat){
-  // Make an img 
-  const image = document.createElement("img");
-  image.src = cat.Image
-const ul = document.getElementById("catList")
-console.log(ul)
-ul.appendChild(image)
-}
-
+// get request , requests data from server 
+//GET PHASE will show the current cats and that is already achieved by fetch method and renderCatfunction 
+// We need to render the text at this point 
+function renderCat(cat) { 
+  const container = document.querySelector('.container');
+  const catObject= document.createElement('div')
+  const catName = document.createElement("h2") // catName first attribute 
+  catName.textContent=cat.Name
+  console.log(catName)
+  catObject.classList.add("catName")// add catName to main catObject 
+  const question = document.createElement("figcaption")  
+  // catQuestion is the second attribute 
+  question.textContent = cat.Question
+  console.log(question)
+  catObject.classList.add("question")// add catQuestion to main catObject 
+  const catImage = document.createElement("img") // catImages is the third attribute 
+  catImage.src = cat.Image 
+  console.log(catImage)
+  catObject.classList.add("catImage")
+ // add catImage to main catObject 
+  catObject.appendChild(catName)
+  catObject.appendChild(question)
+  catObject.appendChild(catImage)
+  container.append(catObject)
+  }
